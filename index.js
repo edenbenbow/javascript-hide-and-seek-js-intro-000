b0d846d557c17b0d20e638a965d3ce2a59f24ec0
E.B <edenbenbow@yahoo.com>
2019-08-13 21:01:38 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ return document.getElementById('nested').querySelector('div.target')
 }
 
 function increaseRankBy(n) {
-  Increases the ranks in all of the .ranked-list by n
-You might need to make use of parseInt
+  for(i = 0; i < n.length; i++) {
+    .ranked-list[i] + n
+  }
 }
