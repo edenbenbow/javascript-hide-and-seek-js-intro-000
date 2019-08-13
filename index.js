b48d2e9ffb9599ad5421b927cbad75015356e07b
E.B <edenbenbow@yahoo.com>
2019-08-13 20:50:36 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,6 +3,6 @@ return document.querySelector(selector);
 }
 
 function nestedTarget() {
-return.document.querySelector('#nested .target')
+return document.getElementById('nested').querySelector('div.target')
 
 }
